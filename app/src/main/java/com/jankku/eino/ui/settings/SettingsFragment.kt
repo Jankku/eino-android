5779eaaedccc86eb82efe5fa9fe0674dc1060dce
Jankku <29043938+Jankku@users.noreply.github.com>
2021-10-10 17:03:17 +0300

    feat: about section to settings screen

--- app/src/main/java/com/jankku/eino/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/jankku/eino/ui/settings/SettingsFragment.kt
@@ -1,12 +1,17 @@
 package com.jankku.eino.ui.settings
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.jankku.eino.BuildConfig
 import com.jankku.eino.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val versionPref: Preference? = findPreference(getString(R.string.app_key))
+        versionPref?.summary = BuildConfig.VERSION_NAME
     }
 }
